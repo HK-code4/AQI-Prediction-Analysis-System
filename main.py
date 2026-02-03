cf39c43b4a468b19014a9579a2ed3294de42b282
HK-code4 <kazmihiba7@gmail.com>
2026-02-04 00:29:18 +0500

    Fix variable name for MongoDB URI

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from pymongo import MongoClient
 import os
 mongo_uri = os.getenv("MONGODB_URI")
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(mongo_uri)
 
 db = client["aqi_db"]
 raw_collection = db["raw_data"]
 feature_collection = db["features"]
 
 print("✅ Connected to MongoDB Atlas")
+
 
