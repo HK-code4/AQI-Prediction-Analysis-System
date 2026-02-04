a47a1a810f9dfeb0f52afd662959b5fc0f9c0b5d
HK-code4 <kazmihiba7@gmail.com>
2026-02-05 02:11:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from pymongo import MongoClient
 
-import os
-mongo_uri = os.getenv("MONGO_URI")
+#import os
+#mongo_uri = os.getenv("MONGO_URI")
 
-client = MongoClient(mongo_uri)
+#client = MongoClient(mongo_uri)
+
+MONGO_URI = "mongodb+srv://kazmihiba22_db_user:OMKVxwdLjXIHYDyQ@cluster0.a2nawnq.mongodb.net/?appName=Cluster0"
+
+client = MongoClient(MONGO_URI)
 
 db = client["aqi_db"]
 raw_collection = db["raw_data"]
 feature_collection = db["features"]
 
 print("✅ Connected to MongoDB Atlas")
+
 
 
 
