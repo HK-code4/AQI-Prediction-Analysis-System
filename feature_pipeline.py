335fc93a072381275ca0178c47528d5b5fe9d419
HK-code4 <kazmihiba7@gmail.com>
2026-02-13 23:48:06 +0500

    Save features to CSV for GitHub Actions
    Added functionality to save features as a CSV for GitHub Actions.

--- feature_pipeline.py
+++ feature_pipeline.py
@@ -56,6 +56,11 @@ def run_feature_pipeline():
 
     print("✅ Features stored in MongoDB Atlas")
 
+    # ---------------- SAVE CSV FOR GITHUB ACTION ----------------
+    df.to_csv("features.csv", index=False)
+    print("✅ Features CSV saved locally for GitHub Actions")
+
 if __name__ == "__main__":
     run_feature_pipeline()
+
 
